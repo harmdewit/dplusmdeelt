815a04d6fa38bf0803e3bd15ebbb304136c26624
Harm de Wit <harmdewit@gmail.com>
2011-05-26 13:31:23 +0100

    stomme capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,26 +2,29 @@ set :application, "dplusm"
 set :host,        "perry.bluerail.nl"
 set :user,        "dplusmdeelt"
 set :use_sudo,    false
-set :deploy_to,   "~/rails"
 set :rake,        "/opt/ruby/bin/rake"
 set :keep_releases,3
-
-set :scm,         :git
-# set :repository,  "ssh://dplusmdeelt@perry.bluerail.nl/~/rails/git/dplusm.git"
-
-set :repository, "git@github.com:harmdewit/DplusM-Deelt.git"
-set :scm_passphrase, ""
-set :branch,      'master' 
-set :scm_verbose, true
-set :deploy_via,  :remote_cache 
-
-
-
 
 role :app, host
 role :web, host
 role :db,  host, :primary => true
 
+# file paths
+set :repository, "#{user}@#{host}:rails/git/#{application}.git" 
+set :deploy_to, "~/rails"
+
+# you might need to set this if you aren't seeing password prompts # default_run_options[:pty] = true
+# As Capistrano executes in a non-interactive mode and therefore doesn't cause # any of your shell profile scripts to be run, the following might be needed # if (for example) you have locally installed gems or applications. Note: # this needs to contain the full values for the variables set, not simply
+# the deltas. # default_environment['PATH']='<your paths>:/usr/local/bin:/usr/bin:/bin' # default_environment['GEM_PATH']='<your paths>:/usr/lib/ruby/gems/1.8'
+
+# miscellaneous options
+set :deploy_via, :remote_cache 
+set :scm, 'git' 
+set :branch, 'master' 
+set :scm_verbose, true
+set :use_sudo, false
+
+# task which causes Passenger to initiate a restart
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
@@ -36,4 +39,3 @@ desc "Link database.yml to production"
 task :link_production_db do
   run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
 end
-
