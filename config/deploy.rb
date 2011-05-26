efc9fa50a761a84992e398a5987fd06d5638e753
Harm de Wit <harmdewit@gmail.com>
2011-05-26 16:27:18 +0100

    minor change

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,29 +2,17 @@ set :application, "dplusm"
 set :host,        "perry.bluerail.nl"
 set :user,        "dplusmdeelt"
 set :use_sudo,    false
+set :deploy_to,   "~/rails"
 set :rake,        "/opt/ruby/bin/rake"
 set :keep_releases,3
+
+set :scm,         :git
+set :repository,  "git@github.com:harmdewit/dplusmdeelt.git" 
 
 role :app, host
 role :web, host
 role :db,  host, :primary => true
 
-# file paths
-set :repository, "#{user}@#{host}:rails/git/#{application}.git" 
-set :deploy_to, "~/rails"
-
-# you might need to set this if you aren't seeing password prompts # default_run_options[:pty] = true
-# As Capistrano executes in a non-interactive mode and therefore doesn't cause # any of your shell profile scripts to be run, the following might be needed # if (for example) you have locally installed gems or applications. Note: # this needs to contain the full values for the variables set, not simply
-# the deltas. # default_environment['PATH']='<your paths>:/usr/local/bin:/usr/bin:/bin' # default_environment['GEM_PATH']='<your paths>:/usr/lib/ruby/gems/1.8'
-
-# miscellaneous options
-set :deploy_via, :remote_cache 
-set :scm, 'git' 
-set :branch, 'master' 
-set :scm_verbose, true
-set :use_sudo, false
-
-# task which causes Passenger to initiate a restart
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
@@ -33,9 +21,26 @@ namespace :deploy do
   end
 
   after "deploy:update_code", :link_production_db
+  after 'deploy:update_code', 'bundler:bundle_new_release'
 end
 
 desc "Link database.yml to production"
 task :link_production_db do
   run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
 end
+
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'vendor_bundle')
+    release_dir = File.join(release_path, 'vendor/bundle')
+    run("mkdir -p #{shared_dir}")
+    run("ln -s #{shared_dir} #{release_dir}")
+  end
+ 
+  task :bundle_new_release, :roles => :app do
+    bundler.create_symlink
+    # run "cd #{release_path}"
+    # run "bundle install --deployment"
+  end
+end
+
