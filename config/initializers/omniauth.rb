4f7dc0109990fda2d4ca2cb6b353bc03b38b44f3
Harm de Wit <harmdewit@gmail.com>
2011-05-30 21:27:48 +0100

    added correct ssl cert directory to omniauth facebook initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     provider :tumblr, 'JxCIF1tJaiqqJmiU4vdK7w0U4OwVuKGQfMgy8yib66b8Lwzma4', 'LScN6lBogvYkZKI80uWS5EfFtAElen3XncXXIrFux1kBmgT2Fy'
   elsif Rails.env.production?
      provider :twitter, 'rN9LiKlovzzbSrWuL89aTw', 'PEs8uvscOfJtGVUSbMySiZtEnM7fF2bW9BPznRlE'
-      provider :facebook, '3a210cc61ff613d5a2447572b20b0339', 'c70b54036fcb8db20e06b28dc504b61a', {:client_options => {:ssl => {:ca_path => "/etc/pki/tls/certs/ca-bundle.crt"}}, :scope => 'manage_pages'}  # Modify this with your SSL certificates path
+      provider :facebook, '3a210cc61ff613d5a2447572b20b0339', 'c70b54036fcb8db20e06b28dc504b61a', {:client_options => {:ssl => {:ca_file => '/etc/pki/tls/certs/ca-bundle.crt'}}, :scope => 'manage_pages'}  # Modify this with your SSL certificates path
       provider :tumblr, 'huH9VMjZ5W2aFYNPnaAUslD39AuMSlxjJTFdbX7xO06rINSwFv', 'CgoYqeFZe2C9E3j1jUsJCRHhmrYBzQZX1IVKnDnk9mDmZ4ik6F'
   end
 
